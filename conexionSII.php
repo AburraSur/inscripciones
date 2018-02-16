310a0e76c1e209bd9c8c7a070a86cad981cf9a09
root <root@desarrollo.ccas.org.co>
2018-02-16 17:14:45 -0500

    Se corrige el usuario de conexion para el servidor de desarrollo

--- conexionSII.php
+++ conexionSII.php
@@ -5,8 +5,8 @@ class conexionsii{
     private $consii;
     
     public function conexionsii(){
-//        $this->consii = new mysqli("192.168.1.26", "consulta_sii", "C0nsult@.123",  "sii_aburra");
-        $this->consii = new mysqli("192.168.1.26", "rrestrepo", "Clave.123",  "sii_aburra");
+        $this->consii = new mysqli("192.168.1.26", "consulta_sii", "C0nsult@.123",  "sii_aburra");
+//        $this->consii = new mysqli("192.168.1.26", "rrestrepo", "Clave.123",  "sii_aburra");
         
         if ($this->consii->connect_error) {
             die("Connection failed: " . $this->consii->connect_error);
